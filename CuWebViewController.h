66b573206b71e126d44c4b824ac7449fc4a38ba1
pomelovico <lwxvero@qq.com>
2018-08-03 19:55:14 +0800

    test

--- CuWebViewController.h
+++ CuWebViewController.h
@@ -16,7 +16,7 @@
 @interface CuWebViewController : UIViewController
 {
 @private
-    JSContext * context;     //js的huanjin
+    JSContext * context;     //js的
     NSString * cuIp;       //CU的地址
     NSString * innerCuUrl;  //内网地址
     UIWebView * myWebView;  //加载网页的view
